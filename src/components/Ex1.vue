a2e08cb96f1daa4f0e2bd5986e01dbea0356e00c
cheekfg897 <gyaltsen.c.2024@computing.smu.edu.sg>
2025-10-28 06:18:24 +0000

    ex1 ex2

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,13 +1,18 @@
 <script>
     //import component
+    import SoundIcon from './subcomponents/SoundIcon.vue';
+
     
     export default { 
         data() {
             return {
                 soundLevel: 0
-            }
-        }
+            };
+        },
+        components: {
+            SoundIcon
     }
+    };
 </script>
 
 <template>
@@ -15,7 +20,7 @@
    Sound level
     <input type="number" min="0" max="3" v-model="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
-    
+    <sound-icon :level="soundLevel"></sound-icon>
 
 </template>
 
--- src/components/subcomponents/BlogPost.vue
+++ src/components/subcomponents/BlogPost.vue
@@ -2,12 +2,34 @@
     export default {
         // Add code if/as necessary
         // Hint: Add properties subject, entry and mood
+        
+        props: {
+            id: String,
+            subject: String,
+            entry: String,
+            mood: String
+        },
+        computed:{
+            imgSrc(){
+                return '/assets/{this.mood.toLowerCase()}.png'
+            }
+        }
+
     }
 </script>
 
 
 <template>
     <!-- TODO: add your template code here. Use boostrap card --> 
-
+        <div class="card">
+            <slot name="img">
+                <img class="card-img-top" src=imgSrc>
+                
+            </slot>
+        <div class="card-body">
+            <h2><slot name="subject"></slot></h2>
+            <p><slot name="entry"></slot></p>
+        </div> 
+    </div>
 </template>
 

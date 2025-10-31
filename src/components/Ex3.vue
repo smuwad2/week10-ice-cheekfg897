a0c169096ca594a83a13f68f0f73849a76fbf185
cheekfg897 <gyaltsen.c.2024@computing.smu.edu.sg>
2025-10-31 12:52:10 +0000

    ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,62 +1,60 @@
 <script setup>
-    // Import BlogPost component
-    import blogPost from './subcomponents/BlogPost2.vue'
-	import axios from 'axios'
+import BlogPost from './subcomponents/BlogPost2.vue'
+import axios from 'axios'
 </script>
 
 <script>
-    export default {
-        data() {
-            return {
-                posts: [] // array of post objects
-            }  
-        },
-        computed: {
-            baseUrl() {
-                if (window.location.hostname == 'localhost')
-                    return 'http://localhost:3000'
-                else {
-                    const codespace_host = window.location.hostname.replace('5173', '3000')
-                    return `https://${codespace_host}`;
-                }
-            }
-        },
-        created() { // created is a hook that executes as soon as Vue instance is created
-            axios.get(`${this.baseUrl}/posts`)
-            .then(response => {
-                // this gets the data, which is an array
-                this.posts = response.data
-                console.log(response.data)
-            })
-            .catch(error => {
-                this.posts = [{ entry: 'There was an error: ' + error.message }]
-            })
-        },
-        methods: {
-            deletePost(id) {
-                // TODO: Complete the delete method
-                axios.get(`${this.baseUrl}/deletePost`,{
-                    params: {
-                        id: id
-                    }}).then(response => {
-                        console.log(response.data.message)
-                        this.posts = this.posts.filter(post => post.id !== id)
-                    }).catch(error => {
-                        console.log(error)
-                    })
-                }
-            }
-        
-        }
-    
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  computed: {
+    baseUrl() {
+      if (window.location.hostname === 'localhost')
+        return 'http://localhost:3000'
+      else {
+        const codespace_host = window.location.hostname.replace('5173', '3000')
+        return `https://${codespace_host}`
+      }
+    }
+  },
+  created() {
+    axios.get(`${this.baseUrl}/posts`)
+      .then(response => {
+        this.posts = response.data
+        console.log(response.data)
+      })
+      .catch(error => {
+        this.posts = [{ entry: 'There was an error: ' + error.message }]
+      })
+  },
+  methods: {
+    deletePost(id) {
+      axios.get(`${this.baseUrl}/deletePost`, {
+        params: { id: id }
+      }).then(response => {
+        console.log(response.data.message)
+        this.posts = this.posts.filter(post => post.id !== id)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
 </script>
 
 <template>
-   <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-    <blog-post v-for="post in posts" :subject="post.subject" :entry="post.entry" :mood="post.mood" :key="post.id">
-        <template v-slot:delete>
-        <button class="btn btn-primary" @click="deletePost(post.id)">Delete</button>
-        </template>
-    </blog-post>
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    :subject="post.subject"
+    :entry="post.entry"
+    :mood="post.mood"
+  >
+    <template #delete>
+      <button class="btn btn-danger" @click="deletePost(post.id)">Delete</button>
+    </template>
+  </BlogPost>
 </template>
-
--- src/components/subcomponents/BlogPost2.vue
+++ src/components/subcomponents/BlogPost2.vue
@@ -1,37 +1,28 @@
 <script>
-    export default {
-        // Add code if/as necessary
-        // Hint: Add properties subject, entry and mood
-        
-        props: {
-            id: String,
-            subject: String,
-            entry: String,
-            mood: String
-        },
-        computed:{
-            imgSrc(){
-                return `/assets/${this.mood.toLowerCase()}.png`
-            }
-        }
-
+export default {
+  props: {
+    id: String,
+    subject: String,
+    entry: String,
+    mood: String
+  },
+  computed: {
+    imgSrc() {
+      return `/assets/${this.mood?.toLowerCase() || 'default'}.png`
     }
+  }
+}
 </script>
 
-
 <template>
-    <!-- TODO: add your template code here Use boostrap card --> 
-    <div class="card">
-            <slot name="img">
-                <img class="card-img-top" :src=imgSrc>
-                
-            </slot>
-            <div class="card-body">
-                <h2>{{ subject }}</h2>
-                <p>{{ entry }}</p>
-            </div> 
-            
-            <slot name ="delete"></slot>
+  <div class="card my-3" style="width: 22rem;">
+    <img class="card-img-top" :src="imgSrc" alt="Mood image" />
+    <div class="card-body">
+      <h5 class="card-title">{{ subject }}</h5>
+      <p class="card-text">{{ entry }}</p>
     </div>
+    <div class="card-footer">
+      <slot name="delete"></slot>
+    </div>
+  </div>
 </template>
-
